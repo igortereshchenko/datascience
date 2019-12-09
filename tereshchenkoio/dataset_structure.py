fcae31bc206660db74978837ecb3aa459a1fcb1f
Your Name <you@example.com>
2019-12-09 15:25:34 +0200

    dgd

--- tereshchenkoio/dataset_structure.py
+++ tereshchenkoio/dataset_structure.py
@@ -1,5 +1,14 @@
 dataset = {
-
-    "Bob":17,
-    "Boba":18
+    'AVpe__e0ilAPnD':{ "count"  :5,
+                       "name"   :"shos",
+                       "reviews"    :5,
+                        },
+    'AVpe__eIOFb55JL': {"count": 54,
+                       "name": "shose",
+                       "reviews": 51,
+                       },
+    'AVpe__e0il45eD': {"count": 500,
+                       "name": "shoses",
+                       "reviews": 500,
+                       }
 }
