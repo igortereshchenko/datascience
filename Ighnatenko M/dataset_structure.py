0a2354ee33d639aff8fa912cbf79c4141c64bee0
Nic <ignatenkolya@gmail.com>
2019-12-10 15:23:47 +0300

    dataset_structure

---
+++ Ighnatenko M/dataset_structure.py
@@ -0,0 +1,13 @@
+dic = {"id": "9996 unique values",
+            "examples -  "
+                        "AVpe__eOilAPnD_xSt-H"
+                        "AVpe__eOilAPnD_xSt-H"
+                        "AVpe__eOilAPnD_xSt-H"
+        "count": "[null] - 100%"
+            "examples - [null]"
+        "dimension": "[null] - 90%"
+                    "6.0 in x 6.0 in x 1.0 - 1%"
+            "examples - 6.0 in x 6.0 in x 1.0 in"
+        "flavours": "[null] - 100%"
+            "examples - [null]"
+         }
