5d2a0be146f7f1909ca672d84abff194e78ccb1f
asfsdfghfgf <58768278+asfsdfghfgf@users.noreply.github.com>
2019-12-11 11:40:58 +0200

    Second Task
    Holenyshchenko S.D., KM-93

---
+++ Holenyshchenko S.D./dataset_structure.py
@@ -0,0 +1,4 @@
+
+dataset = {'747203':{'state':'TX', 'percent_of_beneficials_with_cancer': 0, percent_of_beneficials_with_depression: 62}
+'747016':{'state':'TX', 'percent_of_beneficials_with_cancer': 3, percent_of_beneficials_with_depression: 71}
+'747145':{'state':'TX', 'percent_of_beneficials_with_cancer': 0, percent_of_beneficials_with_depression: 47}}
