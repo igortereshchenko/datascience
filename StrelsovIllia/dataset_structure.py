54bd7220611fc6747b410a097e1a8ccf8eb93b5f
IlliaStrelsov <55940607+IlliaStrelsov@users.noreply.github.com>
2019-12-11 11:41:24 +0200

    Add files via upload
    виконані 1 і 2 завдання

---
+++ StrelsovIllia/dataset_structure.py
@@ -0,0 +1,19 @@
+dataset = {
+
+    "747429":{
+        "agency_name":"SHEPHERD PERSONAL HOME HEALTH"
+        "male_beneficiaries": 26,
+        "female_beneficiaries": 26,
+    },
+
+    "109763":{
+        "agency_name":"NORTH DADE HOME HEALTH AGENCY INC"
+        "male_beneficiaries": 44,
+        "female_beneficiaries": 17,
+    },
+    "237745":{
+        "agency_name":"PROCARE HOMECARE"
+        "male_beneficiaries": 70,
+        "female_beneficiaries": 30,
+    }
+}
