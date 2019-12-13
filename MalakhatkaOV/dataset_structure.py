d13d86f74e70457f27d04fa350ec07467e87b59a
Kyparos <sasha.malakhatka@gmail.com>
2019-12-13 11:10:15 +0200

    dataset_structure

---
+++ MalakhatkaOV/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+    '21' :{
+        'status':True,
+        'mode':'w',
+        'sec_num':'123421'
+    },
+    '22':{
+        'status':False,
+        'mode':'w',
+        'sec_num':'514264'
+    },
+    '23':{
+        'status':False,
+        'mode':'m',
+        'sec_num':'64423234'
+    }
+}
