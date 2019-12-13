99ef5053cd5c3c37b279c6dee0f84072d1aa0fa0
iPotapenko <1vano.potapenko1@gmail.com>
2019-12-13 11:26:32 +0200

    2 tasks completed

---
+++ PotapenkoIV/dataset_structure.py
@@ -0,0 +1,21 @@
+dataset = {
+    "698612":{
+        "cen_year":"2005",
+        "tree_loc":"Assigned",
+        "spc_latin":"PLATANUS ACERIFOLIA",
+        "horz_other":"No"
+    },
+    "860288":{
+        "cen_year":"2006",
+        "tree_loc":"Side",
+        "spc_latin":"ULMUS SPECIES",
+        "horz_other":"No"
+    },
+    "641807":{
+        "cen_year":"2006",
+        "tree_loc":"Median",
+        "spc_latin":"GLEDITSIA TRIACANTHOS",
+        "horz_other":"No"
+    }
+
+}
