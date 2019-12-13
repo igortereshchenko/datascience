669afc6151daafd4e52aa974d704c315de687ce7
Bodia <lalala@gmail.com>
2019-12-13 11:45:08 +0200

    Work is done

---
+++ popovskiiAL/dataset_structure.py
@@ -0,0 +1,22 @@
+dataset = {
+    "698612": {
+                "zip_city": "Bronx",
+                "cb_num": "0",
+                "borocode": "2",
+                "nta_name": "Bronx"
+              },
+    "860288": {
+                "zip_city": "Jamaica",
+                "cb_num": "0",
+                "borocode": "4",
+                "nta_name": "Queens"
+
+              },
+    "641807": {
+                "zip_city": "New York",
+                "cb_num": "0",
+                "borocode": "1",
+                "nta_name": "Manhattan"
+              }
+
+          }
