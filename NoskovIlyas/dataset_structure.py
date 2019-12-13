4d84ee04d13abbbd7527605b4bd0487f61df309b
iNoskov <noskovill01@gmail.com>
2019-12-13 11:16:23 +0200

    I create data set with an explanation to him

---
+++ NoskovIlyas/dataset_structure.py
@@ -0,0 +1,18 @@
+data_set = {
+
+    'zip_city':['Bronx',
+                'Jamaica',
+                'New York'],
+
+    'horz_grate':['no',
+                  'no',
+                  'no'],
+
+    'spc_common':['',
+                  '',
+                  ''],
+
+    'cen_year':['2005',
+                '2006',
+                '2006']
+}
