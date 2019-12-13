09d508f8800c89e2bb17c742552e7d09a588331a
OlenchenkoM <mazsti.o@gmail.com>
2019-12-13 20:22:59 +0200

    commmit

--- dataset_structure.py
+++ dataset_structure.py
@@ -1,21 +1,18 @@
 dataset = {
-    "AVpe__eOilAPnD_xSt-H  ": {
-    "weight : 1 kg " 
-    "name Handcrafted Alpaca Blend 'Purple Charisma ' Sweater (Peru) "
-    "brand - Novica "
-    "dimension - 6.0 in x 6.0 in x 1.0 in 2"
+    "Handcrafted Alpaca Blend 'Purple Charisma' Sweater ": {
+    "weight : 1 kg; " 
+    "brand : Novica; "
+    "dimension : 7; "
     },
-    "AVpe_0ob1cnluZ0-bnWr": {
-        "weight : 1 kg"
-        "name Sof Comfort Women's : Everyday Gel Insoles, 2-pair"
-        "brand BEAUTIFEET"
-        "dimension 5.0 in x 4.0 in x 0.5 in 1"
+    "Sof Comfort Women's Everyday Gel Insoles": {
+        "weight":"1kg; "
+        "brand: BEAUTIFEET; "
+        "dimension: 6; "
     },
-    "AVpe_-0YilAPnD_xStvJ": {
-        "weight 2 kg"
-        "name Marc Fisher Fawna Women Cap Toe Leather Oxford"
-        "brand Marc Fisher "
-        "dimension 6.0 in x 7.0 in x 1 in 1"
+    "Marc Fisher Fawna Women Cap Toe Leather": {
+        "weight":"2 kg; "
+        "brand: Marc Fisher; "
+        "dimension: 9;"
     }
 
 
