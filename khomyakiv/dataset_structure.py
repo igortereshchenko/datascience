3e58bfd9d4ae24eb9955ea469db259c4765e3087
Ihor Khomyak <khomyakihor1@gmail.com>
2019-12-14 23:15:25 +0200

    datasetstructure

---
+++ khomyakiv/dataset_structure.py
@@ -0,0 +1,5 @@
+dataset =[ 
+        {'provider_id': '000000', 'avarage_age': 0, 'male_beneficiares': 0,'female_beneficaries': 0}
+        {'provider_id': '000000', 'avarage_age': 0, 'male_beneficiares': 0,'female_beneficaries': 0}
+        {'provider_id': '000000', 'avarage_age': 0, 'male_beneficiares': 0,'female_beneficaries': 0}
+    ]
