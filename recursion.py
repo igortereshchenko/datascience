1238767c08e4d1d32f34721be26e15d9bc4ba802
IlliaStrelsov <55940607+IlliaStrelsov@users.noreply.github.com>
2019-12-14 15:40:28 +0200

    Add files via upload

--- recursion.py
+++ recursion.py
@@ -1,6 +1,27 @@
-def recursion():
-    if int(male_beneficiaries/female_beneficiaries) > 0.3:
-        print(agency-name,male_beneficiaries/female_beneficiaries*100,male_beneficiaries,female_beneficiaries)
-        return recursion()
+from dataset_structure import dataset
+
+def recursion(dataset):
+    if len(dataset) == 0:
+        return False
+    agency = dataset.pop(next(iter(dataset)))
+    agency_name = agency.get("agency_name")
+    female_beneficiaries = int(agency.get("female_beneficiaries"))
+    male_beneficiaries = int(agency.get("male_beneficiaries"))
+    if male_beneficiaries/female_beneficiaries > 0.3:
+        print(agency_name, male_beneficiaries, female_beneficiaries, male_beneficiaries / female_beneficiaries * 100)
+    recursion(dataset)
+
+print(recursion(dataset))
+
+
+
+
+
+
+
+
+
+
+
 
 
