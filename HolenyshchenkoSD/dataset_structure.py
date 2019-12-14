7d5cb152654f08394f26265c4a2b347b0eba9924
eghnjmkl <58889092+eghnjmkl@users.noreply.github.com>
2019-12-14 11:15:24 -0800

    Create dataset_structure.py

---
+++ HolenyshchenkoSD/dataset_structure.py
@@ -0,0 +1,21 @@
+dataset = {
+    '747203':
+    {
+        'state': 'TX',
+        'percent_of_beneficiaries_with_cancer': 0,
+        'percent_of_beneficiaries_with_depression': 62
+    },
+    '747016':
+    {
+        'state': 'TX',
+        'percent_of_beneficiaries_with_cancer': 3,
+        'percent_of_beneficiaries_with_depression': 71
+    },
+    '747145':
+    {
+        'state': 'TX',
+        'percent_of_beneficiaries_with_cancer': 0,
+        'percent_of_beneficiaries_with_depression': 47
+    },
+
+}
