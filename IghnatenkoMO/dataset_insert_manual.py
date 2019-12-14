df86030028cd1baad7adc6507a7d56cb04591442
Nic <ignatenkolya@gmail.com>
2019-12-14 20:04:41 +0300

    validator

---
+++ IghnatenkoMO/dataset_insert_manual.py
@@ -0,0 +1,8 @@
+from IghnatenkoMO.dataset_structure import dataset
+dataset.update({
+    "Avpe_Oob1cnluZO-bnWr":{
+        "count":["null"],
+        "dimension":["null"],
+        "flavors":["null"]}
+})
+print(dataset)
---
+++ IghnatenkoMO/dataset_structure.py
@@ -0,0 +1,24 @@
+dataset = {
+    "AVpe_0ob1cnluZ0-bnWr":{
+        "count":["null"],
+        "dimension": "6.0 in x 6.0 in x 1.0 in",
+        "flavors": ["null"]},
+    "AVpe__eOilAPnD_xSt-H":{
+        "count":["null"],
+        "dimension":["null"],
+        "flavors":["null"]},
+    "AVpe_54gilAPnD_xSr-B":{
+        "count":["null"],
+        "dimension":" 6.0 in x 6.0 in x 1.0 in",
+        "flavors":["null"]}
+}
+
+
+    
+
+
+
+
+
+
+
