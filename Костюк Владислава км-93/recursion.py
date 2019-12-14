901320b6381b0346c7009443784bedd29c99ad0c
VladaKostiuk <55995703+VladaKostiuk@users.noreply.github.com>
2019-12-14 18:35:40 +0200

    recursion

---
+++ Костюк Владислава км-93/recursion.py
@@ -0,0 +1,7 @@
+def recursion(dataset):
+    for provider_id in dataset.keys():
+        zip_code = int(dataset[provider_id]['zip_code'])
+        avarage_age = int(dataset[provider_id]['avarage_age'])
+
+        if age > 30 and stroke >5:
+            print('Zip_code:' + str(zip_code) + '\nProvider_id:' + str(provider_id) + \nAvarage_age:' + str(avarage_age))
