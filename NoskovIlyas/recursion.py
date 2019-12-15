e295584152680983bd532a29dc75e9299f721faa
iNoskov <noskovill01@gmail.com>
2019-12-15 17:30:50 +0200

    dataset_structure

--- NoskovIlyas/recursion.py
+++ NoskovIlyas/recursion.py
@@ -1,8 +1,11 @@
-from NoskovIlyas.dataset1 import data_set1
+from NoskovIlyas.dataset_structure import data_set
 
 def recursion(info,help):
     if len(info)>=1:
-        if info['Bronx']['cen_year']=='2005':
+        if info[0]=='2005':
             help=help+1
-            return recursion(info.pop(), help)
-recursion(,0)
+            return recursion(info[1:], help)
+        else:
+            return recursion(info[1:],help)
+    return help
+print(recursion(data_set['cen_year'],0))
