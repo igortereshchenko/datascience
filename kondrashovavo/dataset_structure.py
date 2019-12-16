6257d7217349afcaaebb8a264d2666bed943eb80
Lada678 <55762041+Lada678@users.noreply.github.com>
2019-12-16 23:53:18 +0200

    dataset_structure.py

--- kondrashovavo/dataset_structure.py
+++ kondrashovavo/dataset_structure.py
@@ -4,13 +4,13 @@ dataset = {
          'state':'TX',
          'asian_pacific_islander_beneficiaries':'0',
          'hispanic_beneficiaries':' '
-          }
+          },
      '747758':
          {
         'state':'TX',
         'asian_pacific_islander_beneficiaries':'0',
         'hispanic_beneficiaries':'0'
-          }
+          },
      '109795':
           {
         'state':'FL',
