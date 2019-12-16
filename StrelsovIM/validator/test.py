f542b372f1ac195fdbeaee8ea189381698ad8919
IlliaStrelsov <55940607+IlliaStrelsov@users.noreply.github.com>
2019-12-16 23:01:41 +0200

    Add files via upload

---
+++ StrelsovIM/validator/test.py
@@ -0,0 +1,8 @@
+from lib import *
+
+ID_provider = provider_id_validator()
+agency_name = agency_name_validator()
+male = male_beneficiaries_validator()
+female = female_beneficiaries_validator()
+
+print(id, '\n', agency_name, '\n', male, '\n',female)
