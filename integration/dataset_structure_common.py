5267899a049adaf564ead431af29c19c3e81a8b5
OlenchenkoM <mazsti.o@gmail.com>
2019-12-18 20:47:20 +0200

    integration

---
+++ integration/dataset_structure_common.py
@@ -0,0 +1,25 @@
+dataset = {
+    "Handcrafted Alpaca Blend 'Purple Charisma' Sweater ": {
+    "weight : 1 kg; " 
+    "brand : Novica; "
+    "dimension : 7; "
+    'price.amountMin': '62.99'
+    "id : AVpe__eOilAPnD_xSt-H"
+    },
+    "Sof Comfort Women's Everyday Gel Insoles": {
+        "weight":"1kg; "
+        "brand: BEAUTIFEET; "
+        "dimension: 6; "
+        'price.amountMin 30'
+        "id : AVpe_0mrilAPnD_xSqI7"
+    },
+    "Marc Fisher Fawna Women Cap Toe Leather": {
+        "weight":"2 kg; "
+        "brand: Marc Fisher; "
+        "dimension: 9;"
+        'price.amountMin 100'
+        "id : AVpe__JXilAPnD_xSt3F",
+    }
+
+
+}
