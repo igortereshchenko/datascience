4fa06ac012ed903afd85beb51339490b4c0f9d77
Evgen Zaytsev <zond.jeka@gmail.com>
2019-12-19 14:47:15 +0200

    data insert fixed, recursion modified

--- ZaytsevED/dataset_insert_manual.py
+++ ZaytsevED/dataset_insert_manual.py
@@ -1,4 +1,12 @@
 from ZaytsevED.dataset_structure import dataset
-print(dataset)
-dataset['784922'] = {'state': 'NY', 'white_beneficiaries': 64, 'black_beneficiaries': 81}
-print(dataset)
+
+
+def dataset_insert(dataset, prov_id, state, wh_benef, bl_benef):
+    dataset[prov_id] = {
+        'state': state,
+        'white_beneficiaries': wh_benef,
+        'black_beneficiaries': bl_benef
+    }
+
+
+dataset_insert(dataset, '784922', 'NY', 64, 81)
--- ZaytsevED/recursion.py
+++ ZaytsevED/recursion.py
@@ -6,5 +6,4 @@ def recursion(dataset, count=0):
             white_beneficiaries = ' white beneficiaries: ' + str(dataset[lst_keys[count]]['white_beneficiaries']) + ','
             black_beneficiaries = ' black beneficiaries: ' + str(dataset[lst_keys[count]]['black_beneficiaries'])
             print(state, white_beneficiaries, black_beneficiaries)
-        count += 1
-        return recursion(dataset, count)
+        return recursion(dataset, count=count+1)
