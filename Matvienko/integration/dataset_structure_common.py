683d0e0f13f066b10a22d46c594a6a0f1b6d3105
marim <marimat2014@gmail.com>
2019-12-22 01:45:13 +0100

    dataset_structure_common

---
+++ Matvienko/integration/dataset_structure_common.py
@@ -0,0 +1,33 @@
+dataset = {
+    "10471" : {
+        "cen_year":2005,
+        "horz_plant": "No",
+        "vert_wall": "No",
+        "sidw_raise": "No",
+        "cb_num": 0,
+        "wire_2nd": False,
+        "spc-latin":"PLATANUS ACERIFOLIA"
+
+    },
+    "10307	" : {
+        "cen_year": 2005,
+        "horz_plant": "No",
+        "vert_wall": "No",
+        "sidw_raise": "No",
+        "cb_num":0,
+        "wire_2nd": True,
+        "spc-latin":"PYRUS CALLERYANA"
+
+    },
+    "11432" : {
+        "cen_year": 2005,
+        "horz_plant": "No",
+        "vert_wall": "No",
+        "sidw_raise": "Yes",
+        "cb_num":0,
+        "wire_2nd": False,
+        "spc-latin": "ULMUS SPECIES"
+
+    }
+}
+
