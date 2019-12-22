af6ad8f645a13a7e8b96956aeb34ec08e6a97697
egorkravchenko13 <56040497+egorkravchenko13@users.noreply.github.com>
2019-12-22 16:53:20 +0200

    task 6

---
+++ egorkravchenko/dataset_structure_common.py
@@ -0,0 +1,26 @@
+dataset = {
+    "747203": {
+        "zip_code": "77036",
+        "agency_name": "ALL AMERICAN HEALTHCARE INC",
+        "distinct_beneficiaries_non_lupa": "13",
+        "average_number_of_total_visits_per_episode_non_lupa": "13.8",
+        "male_beneficiaries": "0",
+        "female_beneficiaries": "0",
+    },
+    "237745": {
+        "zip_code": "48075",
+        "agency_name": "PROCARE HOMECARE",
+        "distinct_beneficiaries_non_lupa" : "100",
+        "average_number_of_total_visits_per_episode_non_lupa" : "13.1",
+        "male_beneficiaries": "70",
+        "female_beneficiaries": "30",
+    },
+    "747900": {
+        "zip_code": "77071",
+        "agency_name": "NEIGHBOURS HEALTHCARE SERVICES INC",
+        "distinct_beneficiaries_non_lupa" : "25",
+        "average_number_of_total_visits_per_episode_non_lupa": "21.5",
+        "male_beneficiaries": "0",
+        "female_beneficiaries": "0",
+    }
+}
