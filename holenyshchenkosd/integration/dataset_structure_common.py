f8d7a4c517214a45b76b64f67e0d34931860e097
fgjdgjdg <58998971+fgjdgjdg@users.noreply.github.com>
2019-12-22 05:15:17 -0800

    Create dataset_structure_common.py

---
+++ holenyshchenkosd/integration/dataset_structure_common.py
@@ -0,0 +1,30 @@
+dataset = {
+    '747203':
+        {
+            'state': 'TX',
+            'percent_of_beneficiaries_with_cancer': 0,
+            'percent_of_beneficiaries_with_depression': 62,
+            'city': 'HOUSTON',
+            'zipcode': 77036,
+            'total_episodes_non_lupa': 13
+
+        },
+    '747016':
+        {
+            'state': 'TX',
+            'percent_of_beneficiaries_with_cancer': 3,
+            'percent_of_beneficiaries_with_depression': 71,
+            'city': 'RICHMOND',
+            'zipcode': 77469,
+            'total_episodes_non_lupa': 31
+
+        },
+    '747145':
+        {
+            'state': 'TX',
+            'percent_of_beneficiaries_with_cancer': 0,
+            'percent_of_beneficiaries_with_depression': 47,
+            'city': 'HOUSTON',
+            'zipcode': 77071,
+            'total_episodes_non_lupa': 68
+        }}
