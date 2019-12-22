a62fc0ff949403889f62e5fadd771d2e81096745
Olexandra Sydorova <aleksandrasidorova075@gmail.com>
2019-12-22 18:04:41 +0200

    dataset_structure_common

---
+++ SydorovaOS/integration/dataset_structure_common.py
@@ -0,0 +1,29 @@
+dataset = {1 :{
+                        'status': 'old'
+                        ,'inf_shoes': 'No'
+                        ,'trunk_dmg':'None'
+                        ,'horz_plant': 'No'
+                        ,'vert_wall':'No'
+                        ,'sidw_raise':'No'
+                        ,'cen_year':'2005'
+                      }
+           ,2:{
+                         'status': 'old'
+                        ,'inf_shoes': 'No'
+                        ,'trunk_dmg':'None'
+                        ,'horz_plant': 'No'
+                        ,'vert_wall':'No'
+                        ,'sidw_raise':'No'
+                        ,'cen_year':'2006'
+                       }
+           ,3:{
+                         'status': 'new'
+                        ,'inf_shoes': 'No'
+                        ,'trunk_dmg':'None'
+                        ,'horz_plant': 'No'
+                        ,'vert_wall':'Yes'
+                        ,'sidw_raise':'No'
+                        ,'cen_year':'2007'
+                       }
+    }
+
