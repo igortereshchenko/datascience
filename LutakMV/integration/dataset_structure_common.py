e0966990cc22d3231204e5e1b443edb1377be9d8
marin <marina.lutak@gmail.com>
2019-12-22 17:28:25 +0200

    dataset_structure_common

---
+++ LutakMV/integration/dataset_structure_common.py
@@ -0,0 +1,25 @@
+dt = {
+       0: {
+           'PLATANUS ACERIFOLIA':{
+                                  'inf_canopy': 'No',
+                                  'zipcode': 10471,
+                                  'vert_wall': False,
+                                  'wire_2nd': False,
+                                  'cen_year': 2005
+                                 },
+           'ULMUS SPECIES':{
+                             'inf_canopy': 'No',
+                             'zipcode': 11432,
+                             'vert_wall': False,
+                             'wire_2nd': False,
+                             'cen_year': 2006
+                           },
+           'GREDITSIA TRIACANTHOS':{
+                                     'inf_canopy': 'No',
+                                     'zipcode': 10007,
+                                     'vert_wall': False,
+                                     'wire_2nd': False,
+                                     'cen_year': 2006
+                                   }
+           }
+       }
