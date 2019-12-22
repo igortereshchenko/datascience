abdb365e86a5979c6dda88bbd2d008c6bcd64c97
eugene <sazoneugene04@gmail.com>
2019-12-22 17:13:27 +0200

    dataset_common_insert

--- Sazonenko/validator/lib.py
+++ Sazonenko/validator/lib.py
@@ -1,19 +1,22 @@
 import re
 
 
-def spc_latin_validator(text: str) -> bool:
+def spc_latin_validator(text: str):
     text = text.replace(" ", "")
-    return bool(re.fullmatch(r"^[\w,][^0-9]+$", text))
+    if bool(re.fullmatch(r"^[\w,][^0-9]+$", text)):
+        return text
 
 
-def wire_htap_validator(text: str) -> bool:
-    return bool(re.fullmatch(r"^(yes|no)$", text))
+def wire_htap_validator(text: str):
+    if bool(re.fullmatch(r"^(yes|no)$", text)):
+        return text
 
 
-def status_validator(text: str) -> bool:
+def status_validator(text: str):
+    if bool(re.fullmatch(r"^(excellent|bad|good|dead|poor)$", text)):
+        return text
 
-    return bool(re.fullmatch(r"^(excellent|bad|good|dead|poor)$", text))
 
-
-def borocode_validator(text: str) -> bool:
-    return bool(re.fullmatch(r"^\d$", text))
+def borocode_validator(text: str):
+    if bool(re.fullmatch(r"^\d$", text)):
+        return text
