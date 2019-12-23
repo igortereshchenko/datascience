7268b211ffaa28f2b796e8361acaeaba147714fa
nazarStarenchenko <nazarstarenchenko@gmail.com>
2019-12-23 18:27:01 +0200

    changed names of files

---
+++ StarenchenkoNV/integration/dataset_structure_common.py
@@ -0,0 +1,29 @@
+new_dataset = {
+    "New York": {
+        "cb-num": "0"
+        ,"cen_year": "2005"
+        ,"wire_prime":"No"
+        ,"sidw_crack":"No"
+        ,"status":"Good"
+        ,"inf_shoes" : "No"
+
+    }
+    ,'Jamaica': {
+    "cb-num": "0"
+    ,"cen_year": "2006"
+    ,"wire_prime":"No"
+    , "sidw_crack":"No"
+    , "status":"Good"
+    , "inf_shoes":"No"
+
+    }
+    ,'Bronx': {
+         "cb-num": "0"
+        ,"cen_year": "2005"
+        ,"wire_prime": "No"
+        ,"sidw_crack":"No"
+        ,"status":"Good"
+        ,"inf_shoes":"No"
+    }
+}
+
