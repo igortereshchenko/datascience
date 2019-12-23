fb40edef738a217a1cf4fed874489261f1257db2
artemoliinyk <a.oliinyk-2023@kpi.ua>
2019-12-23 09:08:41 +0200

    dataset_structure_common, dataset_common_insert

--- oliinykao/validator/lib.py
+++ oliinykao/validator/lib.py
@@ -1,12 +1,24 @@
 import re
-def id_validator(id):
-    return bool(re.match("^AVpe\_.{15}$", id))
+def id_validator(prompt):
+    id = input(prompt)
+    while not bool(re.match("^AVpe.{16}$", id)):
+        id = input(prompt)
+    return id
 
-def brand_validator(brand):
-    return bool(re.match("^\w*(\s\w*)*$", brand))
+def brand_validator(prompt):
+    brand = input(prompt)
+    while not bool(re.match("^\w*(\s\w*)*$", brand)):
+        brand = input(prompt)
+    return brand
 
-def colors_validator(colors):
-    return bool(re.match("^\w*(.?\s\w*)*$", colors))
+def colors_validator(prompt):
+    colors = input(prompt)
+    while not bool(re.match("^\w*(.?\s\w*)*$", colors)):
+        colors = input(prompt)
+    return colors
 
-def flavors_validator(flavors):
-    return bool(re.match("^\w*(\s\w*)*$", flavors))
+def flavors_validator(prompt):
+    flavors = input(prompt)
+    while not bool(re.match("^\w*(\s\w*)*$", flavors)):
+        flavors = input(prompt)
+    return flavors
