63f0c93fbca91c3b28e28aa62771218c2f3539ae
dasha-ryzhkova <ryzhkovadasha2002@.gmail.com>
2019-12-23 02:56:57 +0200

    dataset_structure_common

---
+++ RyzhkovaDO/integration/​dataset_structure_common.py
@@ -0,0 +1,32 @@
+dataset = {
+           698612:{
+               2005:{
+                'vert_wall': "No"
+                ,'zipcode': 10471
+                ,'borocode': 2
+                ,'zip_city': "Bronx"
+                ,'horz_grate': "No"
+                ,'spc_common': "London Planetree"
+                     }
+                  }
+           ,860288:{
+                2006:{
+                'vert_wall': "No"
+                ,'zipcode': 11432
+                ,'borocode': 4
+                ,'zip_city': "Jamaica"
+                ,'horz_grate': "No"
+                ,'spc_common': "Elm, oter"
+                     }
+                   }
+           ,641807:{
+                2006:{
+                'vert_wall': "No"
+                ,'zipcode': 10007
+                ,'borocode': 1
+                ,'zip_city': "New York"
+                ,'horz_grate': "No"
+                ,'spc_common': "Honeylocust"
+                     }
+                   }
+           }
