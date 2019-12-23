aac571e560b45367f22e27cd6aa8aa9bf5658c4f
nazarStarenchenko <nazarstarenchenko@gmail.com>
2019-12-23 18:20:57 +0200

    commiting creation of integration package

---
+++ StarenchenkoNV/integration/dataset_common_insert.py
@@ -0,0 +1,29 @@
+new_dataset = {
+    "New York": {
+
+        "cen_year": "2005"
+        ,"wire_prime":"No"
+        ,"sidw_crack":"No"
+        ,"status":"Good"
+        ,"inf_shoes" : "No"
+
+    }
+    ,'Jamaica': {
+
+    "cen_year": "2006"
+    ,"wire_prime":"No"
+    , "sidw_crack":"No"
+    , "status":"Good"
+    , "inf_shoes":"No"
+
+    }
+    ,'Bronx': {
+
+        "cen_year": "2005"
+        ,"wire_prime": "No"
+        ,"sidw_crack":"No"
+        ,"status":"Good"
+        ,"inf_shoes":"No"
+    }
+}
+
